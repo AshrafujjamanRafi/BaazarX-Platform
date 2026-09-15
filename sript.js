d679e8bdf2947e2c3bc6577457deaff53bd82d16
Ashrafujjaman Rafi <aashrafrafi16@gmail.com>
2026-09-15 23:08:17 +0600

    yes

--- sript.js
+++ sript.js
@@ -4,43 +4,49 @@ const products=[
         name:"Stylish headphone",
         price:790,
         category:"electronics",
-        image:"headphone.jpg"
+        image:"headphone.jpg",
+        Description:"A stylish headphone for everyday use"
     },
     {
         id:2,
         name:"Nike shoes",
         price:7900,
         category:"shoes",
-        image:"shoes.jpg"
+        image:"shoes.jpg",
+        Description:"A stylish Nike shoe for everyday use"
     },
     {
         id:3,
         name:"White T-shirt",
         price:500,
         category:"clothing",
-        image:"tshirt.jpg"
+        image:"tshirt.jpg",
+         Description:"A stylish T-shirt for everyday use"
     },
     {
         id:4,
         name:"Wallet",
         price:1000,
         category:"accessories",
-        image:"wallet.jpg"
+        image:"wallet.jpg",
+         Description:"A stylish wallet for everyday use"
     },
     {
         id:5,
         name:"Speaker",
         price:1500,
         category:"accessories",
-        image:"speaker.jpg"
+        image:"speaker.jpg",
+         Description:"A stylish speaker for everyday use"
     },
     {
         id:6,
         name:"Rolex Watch",
         price:3999,
         category:"clothing",
-        image:"watch.jpg"
-    },
+        image:"watch.jpg",
+         Description:"A stylish watch for everyday use"
+    }
 ]
 const productList=document.getElementById("products")
 let count=0;
@@ -57,12 +63,21 @@ function productDisplay(temp){
         const card=document.createElement("div")
         card.className="product-card"
         productList.appendChild(card)
+        card.addEventListener("click",function(event){
+              if(event.target.closest(".add-cart")){
+                return;
+            }
+            else{
+                showDetails(element.id)
+            }
+        })
         const image=document.createElement("img")
         image.src=element.image
         card.appendChild(image)
         const productName=document.createElement("p")
         productName.textContent=element.name
         card.appendChild(productName)
+        
         const productPrice=document.createElement("p")
         productPrice.textContent="৳"+ element.price
         card.appendChild(productPrice)
@@ -207,4 +222,46 @@ checkoutButton.addEventListener("click",function(){
     cartCount.innerHTML=`${count}`
     document.getElementById("cart").classList.remove("active")
 })
+const productDisplayBox=document.getElementById("productDisplay")
+const detailsImage=document.getElementById("detailsImage")
+const detailsName=document.getElementById("detailsName")
+const detailsPrice=document.getElementById("detailsPrice")
+const detailsCategory=document.getElementById("detailsCategory")
+const detailsDescription=document.getElementById("detailsDescription")
+const closeDetails=document.getElementById("closeDetails")
+const detailsCartButton=document.getElementById("detailsCartButton")
+function showDetails(id){
+        const product=products.find(element=>element.id==id)
+        detailsImage.src=product.image
+    detailsName.textContent=product.name
+    detailsPrice.textContent="৳"+product.price
+    detailsCategory.textContent=product.category
+    detailsDescription.textContent=product.Description
+    productDisplayBox.style.display="flex"
+    detailsCartButton.dataset.id = id
+}
+closeDetails.addEventListener("click",function(){
+     productDisplayBox.style.display="none"
+})
+detailsCartButton.addEventListener("click",function(){
+            const id=detailsCartButton.dataset.id
+            const singleProduct=products.find(element=>element.id==id)
+            const existingProduct=productArray.find(element=>element.id == id)
+            if(existingProduct){
+                existingProduct.quantity+=1
+            }
+            else{
+            const cartProduct={...singleProduct,quantity:1}
+            productArray.push(cartProduct)
+            }
+            count+=1 
+            localStorage.setItem("cart",JSON.stringify(productArray))
+             const cartCount=document.getElementById("cartCount")
+        cartCount.innerHTML=`${count}`
+     })
+     const interfaceHome=document.getElementById("interface-home")
+     interfaceHome.addEventListener("click",function(){
+        takeInput.value=""
+        productDisplay(products)
+     })
 productDisplay(products)
